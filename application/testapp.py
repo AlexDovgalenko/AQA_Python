078da1e1f66840086b33679cb64fd0f359d12954
Alexander Dovgalenko <adovgalenko@lohika.com>
2018-08-27 17:36:23 +0300

    commit changes for task 1

---
+++ application/testapp.py
@@ -0,0 +1,24 @@
+def fibgen(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibgen(n-1) + fibgen(n-2)
+
+
+def generate_fibonacci(length):
+
+    result = []
+
+    if length == 0:
+        result = [0]
+    elif length == 1:
+        result = [0, 1]
+    else:
+        for i in range(length):
+            result.append(fibgen(i))
+    return result
+
+
+print(generate_fibonacci(12))
