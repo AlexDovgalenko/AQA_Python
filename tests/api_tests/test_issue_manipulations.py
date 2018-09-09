4b4d24c5ba0e7f11556bc85a2d7e63a2a7c0d9b0
Alexander Dovgalenko <adovgalenko@lohika.com>
2018-09-09 13:40:55 +0300

    Added test to update issue via rest api

--- tests/api_tests/test_issue_manipulations.py
+++ tests/api_tests/test_issue_manipulations.py
@@ -16,26 +16,25 @@ headers = {
     }
 my_path = os.path.abspath(os.path.dirname('../../__file__'))
 path_to_payload_create = os.path.join(my_path, "test_data/jsons/create_issue_payloads")
+path_to_payload_update = os.path.join(my_path, "test_data/jsons/update_issue_payloads")
 
-issues_id_list = []
+# global variable to store Issue ID created in JIRA
+issue_id = ''
+
 
 @pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_create))
 def test_create_issue(payload, expected_status_code):
-    # print(type(common_utils.get_current_datetime_str()))
-    # print(common_utils.get_current_datetime_str())
-    # print(type(payload))
     datetime = common_utils.get_current_datetime_str()
     payload_fin = payload % datetime
-    # print("payload is: " + payload)
-    # print("formatted payload is: " + payload_fin)
     r = requests.request("POST", baseUrl+api_url, data=payload_fin, headers=headers, auth=(username, password))
     assert r.status_code == int(expected_status_code)
     if 'id' in r.json():
-        issues_id_list.append(r.json()['id'])
+        global issue_id
+        issue_id = r.json()['id']
 
 
-# @pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_update))
-# @pytest.mark.parametrize("issues_id_list", issues_id_list)
-# def test_update_issue(issues_id_list, payload, expected_status_code):
-#     r = requests.request("POST", baseUrl+api_url, data=payload, headers=headers, auth=(username, password))
-#     assert r.status_code == int(expected_status_code)
+@pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_update))
+def test_update_issue(payload, expected_status_code):
+    global issue_id
+    r = requests.request("PUT", baseUrl+api_url+'/'+issue_id, data=payload, headers=headers, auth=(username, password))
+    assert r.status_code == int(expected_status_code)
