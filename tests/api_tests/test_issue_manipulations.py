0bccdca476f21b1d04cab361d9d72cf481e61a03
Alexander Dovgalenko <adovgalenko@lohika.com>
2018-09-09 12:46:20 +0300

    adding issue uniqueness by enriching summary with current date time

--- tests/api_tests/test_issue_manipulations.py
+++ tests/api_tests/test_issue_manipulations.py
@@ -5,6 +5,7 @@ import logging
 import pytest
 import os.path
 
+
 logger = logging.getLogger()
 baseUrl = config.Config.JIRA_BASE_URL
 api_url = config.Config.JIRA_REST_API_URL
@@ -16,9 +17,25 @@ headers = {
 my_path = os.path.abspath(os.path.dirname('../../__file__'))
 path_to_payload_create = os.path.join(my_path, "test_data/jsons/create_issue_payloads")
 
+issues_id_list = []
 
 @pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_create))
 def test_create_issue(payload, expected_status_code):
-    print(payload)
-    r = requests.request("POST", baseUrl+api_url, data=payload, headers=headers, auth=(username, password))
+    # print(type(common_utils.get_current_datetime_str()))
+    # print(common_utils.get_current_datetime_str())
+    # print(type(payload))
+    datetime = common_utils.get_current_datetime_str()
+    payload_fin = payload % datetime
+    # print("payload is: " + payload)
+    # print("formatted payload is: " + payload_fin)
+    r = requests.request("POST", baseUrl+api_url, data=payload_fin, headers=headers, auth=(username, password))
     assert r.status_code == int(expected_status_code)
+    if 'id' in r.json():
+        issues_id_list.append(r.json()['id'])
+
+
+# @pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_update))
+# @pytest.mark.parametrize("issues_id_list", issues_id_list)
+# def test_update_issue(issues_id_list, payload, expected_status_code):
+#     r = requests.request("POST", baseUrl+api_url, data=payload, headers=headers, auth=(username, password))
+#     assert r.status_code == int(expected_status_code)
--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,12 +1,25 @@
 import string
 import random
 import datetime
+import os.path
 
 
 def rnd_string_gen(length):
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
 
 
-def get_current_datetime():
+def get_current_datetime_str():
     return ''.join(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+
+def read_issue_data_from_csv(file_path):
+    with open(file_path) as f:
+        payloads_data = []
+        for row in f:
+            payloads_data.append(str(row).rstrip('\n').split('|'))
+    return payloads_data
+
+if __name__ == '__main__':
+    my_path = os.path.abspath(os.path.dirname('../__file__'))
+    path_to_csv = os.path.join(my_path, "test_data/jsons/create_issue_payloads")
+    print(read_issue_data_from_csv(path_to_csv))
