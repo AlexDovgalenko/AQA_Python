bf8b84163da80a9263098637de7a2d8d9b23c664
Alexander Dovgalenko <adovgalenko@lohika.com>
2018-09-11 12:53:34 +0300

    added function to delete issue after all tests are passed, added test to delete all issues, created by a certain user added conftest.py file for logging fixture comfort re-use

---
+++ tests/api_tests/test_delete_all_my_issues.py
@@ -0,0 +1,38 @@
+import requests
+from configurations import config
+from utils import common_utils
+import pytest
+import os.path
+
+os.path.dirname('../../__file__')
+
+baseUrl = config.Config.JIRA_BASE_URL
+api_url = config.Config.JIRA_REST_API_URL
+username = config.Config.JIRA_USERNAME
+password = config.Config.JIRA_PASS
+headers = {
+    'Content-Type': "application/json",
+}
+# # global variable to store Issue ID created in JIRA
+# issue_id = ''
+
+
+@pytest.fixture()
+def get_issue_list_by_user_id(user_mame):
+    payload = '{"jql":"reporter = %s","startAt":0, "maxResults": 1000, "fields":["id"]}' % user_mame
+    r = requests.request("POST", baseUrl + api_url + '/search', data=payload, headers=headers, auth=(username, password))
+    issue_id_list = []
+    for i in r.json().get('issues'):
+        issue_id_list.append(i['id'])
+    return issue_id_list
+
+
+@pytest.mark.skip
+@pytest.mark.parametrize("issue_id", get_issue_list_by_user_id(username))
+def test_delete_issues(issue_id, logger):
+    """
+    Comment "@pytest.mark.skip" decorator for "test_delete_issues" to delete all issues created by desired username
+    """
+    global username
+    requests.request("DELETE", baseUrl+api_url+"/issue/"+issue_id, headers=headers, auth=(username, password))
+    logger.debug("deleting issue with ID# {}".format(issue_id))
--- tests/api_tests/test_issue_manipulations.py
+++ tests/api_tests/test_issue_manipulations.py
@@ -1,48 +1,63 @@
 import requests
 from configurations import config
 from utils import common_utils
-import logging
+# import logging
 import pytest
 import os.path
 
-os.path.dirname('../../__file__')
 
-logger = logging.getLogger()
-baseUrl = config.Config.JIRA_BASE_URL
-api_url = config.Config.JIRA_REST_API_URL
-username = config.Config.JIRA_USERNAME
-password = config.Config.JIRA_PASS
-headers = {
-    'Content-Type': "application/json",
-    }
-my_path = os.path.abspath(os.path.dirname('../../__file__'))
-path_to_payload_create = os.path.join(my_path, "test_data/jsons/create_issue_payloads")
-path_to_payload_update = os.path.join(my_path, "test_data/jsons/update_issue_payloads")
-path_to_payload_search = os.path.join(my_path, "test_data/jsons/search_issue_payloads")
+class TestIssueManipulations():
 
-# global variable to store Issue ID created in JIRA
-issue_id = ''
+    os.path.dirname('../../__file__')
 
+    # logger = logging.getLogger()
+    baseUrl = config.Config.JIRA_BASE_URL
+    api_url = config.Config.JIRA_REST_API_URL
+    username = config.Config.JIRA_USERNAME
+    password = config.Config.JIRA_PASS
+    headers = {
+        'Content-Type': "application/json",
+        }
+    # global variable to store Issue ID created in JIRA
+    issue_id = ''
 
-@pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_create))
-def test_create_issue(payload, expected_status_code):
-    datetime = common_utils.get_current_datetime_str()
-    payload_fin = payload % datetime
-    r = requests.request("POST", baseUrl+api_url+"/issue", data=payload_fin, headers=headers, auth=(username, password))
-    assert r.status_code == int(expected_status_code)
-    if 'id' in r.json():
+    @pytest.fixture(scope='module', autouse=True)
+    def payload_type(payload_type):
+        my_path = os.path.abspath(os.path.dirname('../../__file__'))
+        payload = []
+        if payload_type == 'create':
+            path = os.path.join(my_path, "test_data/jsons/create_issue_payloads")
+            payload = common_utils.read_issue_data_from_csv(path)
+        elif payload_type == 'update':
+            path  = os.path.join(my_path, "test_data/jsons/update_issue_payloads")
+            payload = common_utils.read_issue_data_from_csv(path)
+        elif payload_type == 'search':
+            path = os.path.join(my_path, "test_data/jsons/search_issue_payloads")
+            payload = common_utils.read_issue_data_from_csv(path)
+        return payload
+
+    def teardown_class(self):
+        # logger.debug("deleting issue with ID: {}".format(issue_id))
+        requests.request("DELETE", self.baseUrl+self.api_url+"/issue/"+issue_id, headers=self.headers, auth=(self.username, self.password))
+
+    @pytest.mark.parametrize("payload, expected_status_code", payload_type("create"))
+    def test_create_issue(self, payload, expected_status_code):
+        datetime = common_utils.get_current_datetime_str()
+        payload_fin = payload % datetime
+        r = requests.request("POST", self.baseUrl+self.api_url+"/issue", data=payload_fin, headers=self.headers, auth=(self.username, self.password))
+        assert r.status_code == int(expected_status_code)
+        if 'id' in r.json():
+            global issue_id
+            issue_id = r.json()['id']
+
+    @pytest.mark.dependency(depends = ['test_create_issue'])
+    @pytest.mark.parametrize("payload, expected_status_code", payload_type("update"))
+    def test_update_issue(self, payload, expected_status_code):
         global issue_id
-        issue_id = r.json()['id']
+        r = requests.request("PUT", self.baseUrl+self.api_url+'/issue/'+issue_id, data=payload, headers=self.headers, auth=(self.username, self.password))
+        assert r.status_code == int(expected_status_code)
 
-
-@pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_update))
-def test_update_issue(payload, expected_status_code):
-    global issue_id
-    r = requests.request("PUT", baseUrl+api_url+'/issue/'+issue_id, data=payload, headers=headers, auth=(username, password))
-    assert r.status_code == int(expected_status_code)
-
-
-@pytest.mark.parametrize("payload, expected_status_code", common_utils.read_issue_data_from_csv(path_to_payload_search))
-def test_search_for_issue(payload, expected_status_code):
-    r = requests.request("POST", baseUrl+api_url+'/search', data=payload, headers=headers, auth=(username, password))
-    assert r.status_code == int(expected_status_code)
+    @pytest.mark.parametrize("payload, expected_status_code", payload_type("search"))
+    def test_search_for_issue(self, payload, expected_status_code):
+        r = requests.request("POST", self.baseUrl+self.api_url+'/search', data=payload, headers=self.headers, auth=(self.username, self.password))
+        assert r.status_code == int(expected_status_code)
